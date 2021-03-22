4855e99ef4332d4bc4ff9829bc1fbeca40507d8e
daniel <dannymints@gmail.com>
2021-03-22 01:36:19 -0400

    Set with Han on zoom call, here we fixed some frontend issues together and added some new things

--- python_project/apps/products_db/models.py
+++ python_project/apps/products_db/models.py
@@ -5,7 +5,13 @@ from django.db import models
 from django.urls import reverse
 
 
+# every product needs to have its guid and when the user will be adding products into the cart
+# the front end service will store the products in an array of guids
+# to google: django guid, django make dynamic page for product with guid, django make shop cart
 class Products(models.Model):
     product_name = models.CharField(max_length=120)
     category = models.CharField(max_length=120)
+    # models.IntegerField
     price = models.CharField(max_length=120)
+    # here we create a field called image
+    # inevtory
